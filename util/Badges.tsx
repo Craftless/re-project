7071b5d0f85e4d8064fd778092cc7f71e7feb769
Craftless <javier.tzh@gmail.com>
2022-05-02 11:58:16 +0800

    Added react native svg transformer

--- util/Badges.tsx
+++ util/Badges.tsx
@@ -1,2 +1,15 @@
-import Star_athelete from "../assets/badges/images/star_athelete.svg";
+import star_athelete from "../assets/badges/images/star_athelete.svg";
+import fire from "../assets/badges/images/fire.svg";
+import { SvgProps } from "react-native-svg";
 
+const badges: any = {
+  star_athelete: star_athelete,
+  fire: fire,
+}
+
+function Badge({badgeId, width, height}: {badgeId: any, width: number, height: number}) {
+  const BadgeIcon: React.FC<SvgProps> = badges[badgeId];
+  return <BadgeIcon width={width} height={height} />
+}
+
+export default Badge;
